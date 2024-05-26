493262ece6b8b1ea48d831987b67b8af81de1369
mooghen2s <167309023+mooghen2s@users.noreply.github.com>
2024-05-26 17:28:03 +0700

    [skip ci]

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
+// index.js
 const createMarion = require("./marions-funny-cat-videos");
 
 const YOUTUBE_API_KEY = process.env.YOUTUBE_API_KEY;
+const SEARCH_QUERY = process.env.SEARCH_QUERY || 'funny cat'; // Default to 'funny cat' if not set
+const COUNT = parseInt(process.env.COUNT, 10) || 5; // Default to 5 if not set or not a valid number
 
 if (!YOUTUBE_API_KEY) {
     console.error("YOUTUBE_API_KEY is not set.");
@@ -11,7 +14,7 @@ if (!YOUTUBE_API_KEY) {
 const marion = createMarion(YOUTUBE_API_KEY);
 
 // Get some funny cat videos
-marion.getFunnyCatVideos(500)
+marion.getFunnyCatVideos(SEARCH_QUERY, COUNT)
     .then(videoList => {
         console.log("videoList", videoList);
     })
@@ -20,4 +23,4 @@ marion.getFunnyCatVideos(500)
     });
 
 // Export CSV
-marion.exportFunnyCatVideosCSV("funny_cat_videos.csv", 500);
+marion.exportFunnyCatVideosCSV("funny_cat_videos.csv", SEARCH_QUERY, COUNT);
