b193c5f40581bbfe6ab1a053930fb7d6abeb1259
mooghen2s <167309023+mooghen2s@users.noreply.github.com>
2024-06-11 22:23:44 +0700

    [skip ci]

--- main.py
+++ main.py
@@ -1,5 +1,27 @@
-from webui import webui
+import subprocess
+import time
+import pyautogui
 
-MyWindow = webui.window()
-MyWindow.show('<html><script src="webui.js"></script> Hello World! </html>')
-webui.wait()
+try:
+    Url_browser = 'https://www.youtube.com/watch?v=Pv5jRSqSzNE'
+    profile_dir = 'Profile2'
+    window_size = '300,300'
+    command = [
+        "C:/Program Files/Google/Chrome/Application/chrome.exe",
+        f"--user-data-dir=C:/ai/browser/profiles/{profile_dir}",
+        f"--profile-directory={profile_dir}",
+        f"--app={Url_browser}",
+    ]
+    chrome_process = subprocess.Popen(command)
+except Exception as e:
+    print(f'Error on opening URL and profile: {e}')
+    exit(1)
+
+# Tunggu selama 30 detik
+time.sleep(5)
+pyautogui.press('f11') 
+# Tangkap layar dan simpan sebagai PNG
+screenshot = pyautogui.screenshot()
+screenshot.save("screenshot.png")
+
+print("Screenshot saved as screenshot.png")
